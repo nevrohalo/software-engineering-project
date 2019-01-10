10e4f4a716a532cf2dc13b22ca3bfa4c69e77859
nevrohalo <>
2019-01-10 22:53:11 +0100

    Task 3 - Finish the Index and About Us page

--- school-event-planner/resources/views/pages/about.blade.php
+++ school-event-planner/resources/views/pages/about.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts/standard-layout')
 
 @section('content')
-    <h1>Page - {{$title}}</h1>
-    <p>Work in Progress</p>
+    <!-- Picture Logo -->
+    <header class="background-logo">
+        <div class="background-logo-content">
+            <a href="/">
+                <img class="logo-image" src="../pics/logo-icon.png">
+            </a>
+            <div class="logo-title">
+                School Event Planner
+            </div>
+            <div class="logo-text">
+                A Project by Nevro Halo
+            </div>
+        </div>
+    </header>
 @endsection
--- school-event-planner/resources/views/pages/index.blade.php
+++ school-event-planner/resources/views/pages/index.blade.php
@@ -1,6 +1,66 @@
 @extends('layouts/standard-layout')
 
 @section('content')
-    <h1>Page - {{$title}}</h1>
-    <p>Work in Progress</p>
+    <!-- Wrapper -->
+    <section id="portal-wrapper-id" class="portal">
+        <div class="portal-layer portal-layer-bottom">
+            <div class="portal-layer-wrap">
+                <div class="portal-layer-body">
+                    <h2>Organizer Portal</h2>
+                    <p>Log in through our organizer portal</p>
+                </div>
+                <div class="portal-layer-note">
+                    <h2><a href="/organizer-portal">Click Here</a></h2>
+                </div>
+                <img class="portal-image" src="../pics/organizer-login-icon.png" alt="Icon not found">
+            </div>        
+        </div>
+
+        <div class="portal-layer portal-layer-top">
+            <div class="portal-layer-wrap">
+                <div class="portal-layer-body">
+                    <h2>Student Portal</h2>
+                    <p>Log in through our student portal</p>
+                </div>
+                <div class="portal-layer-note">
+                    <h2><a href="/student-portal">Click Here</a></h2>
+                </div>
+                <img class="portal-image" src="../pics/student-login-icon.png" alt="Icon not found">
+            </div>
+        </div>
+
+        <div class="portal-handle"></div>
+    </section>
+    
+    <!-- Footer -->
+    <div class="portal-footer">
+        <ul class="portal-footer-bar">
+            <li><a id="footer-logo" href="https://github.com/nevrohalo/software-engineering-project" target="_blank">School Event Planner by Nevro Halo</a></li>
+            <li><a href="/student-portal">Student Portal</a></li>
+            <li><a href="/organizer-portal">Organizer Portal</a></li>
+            <li><a href="/about">About Us</a></li>
+        </ul>
+    </div>
+
+    <!-- TODO: Embedd script -->
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            let wrapper = document.getElementById('portal-wrapper-id');
+            let layer = wrapper.querySelector('.portal-layer-top');
+            let handle = wrapper.querySelector('.portal-handle');
+
+            let skew = 0;
+            let delta = 0;
+
+            if (wrapper.className.indexOf('portal') != -1) {
+                skew = 1000;
+            }
+
+            wrapper.addEventListener('mousemove', function(e) {
+                delta = (e.clientX - window.innerWidth / 2) * 0.5;
+                handle.style.left = e.clientX + delta + 'px';
+                layer.style.width = e.clientX + delta + skew + 'px';
+            });
+        });
+    </script>
 @endsection
